ac2dce3d8007692dd28f0d8bb01f8e526eac6593
kaitai <kaitgit@outlook.com>
2019-03-20 09:57:35 +0200

    Added doc strings for methods

--- bot/race_interface.py
+++ bot/race_interface.py
@@ -6,31 +6,36 @@ from abc import abstractmethod
 
 class Race_macro(abc.ABC):
 
-    @abstractmethod
     def __init__(self, controller):
-        raise NotImplementedError('Method __init__ not implemented')
+        super().__init__()
 
     @abstractmethod
-    def train_unit(self, goal, unit):
+    def train_unit(self, building_typeID, unit_typeID):
+        """Trains given units on given buildings."""
         raise NotImplementedError('Method train_unit not implemented')
 
     @abstractmethod
     def general_macro(self):
+        """Takes care of general macro for the bot."""
         raise NotImplementedError('Method general_macro not implemented')
 
     @abstractmethod
     def early_tech(self):
+        """Sets early game tech goals."""
         raise NotImplementedError('Method early_tech not implemented')
 
     @abstractmethod
     def mid_tech(self):
+        """Sets middle game tech goals."""
         raise NotImplementedError('Method mid_tech not implemented')
 
     @abstractmethod
     def late_tech(self):
+        """Sets late game tech goals."""
         raise NotImplementedError('Method late_tech not implemented')
 
     @abstractmethod
     def air_tech(self):
+        """Sets air tech goals."""
         raise NotImplementedError('Method air_tech not implemented')
 
